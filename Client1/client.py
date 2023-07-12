3a81aabbb028c4a83924999f10a88d277181e19b
Adam Polywkan <82613399+Apolywkan@users.noreply.github.com>
2023-07-11 23:34:39 -0600

    Initial commit
    Includes:
    
    key_generator.py:
    python script to generate private and public keys for server and clients 1-5 using RSA
    
    client.py:
    stored in the Client1 directory used for communicating with server
    
    server.py:
    stored in the Server directory used for hosting the email server
    
    client and server files are completed up to and including when the menu is generated. Files initially communicate using RSA private and public keys and then switch to AES ECB once the symmetric key is generated and transferred to a validated user

---
+++ Client1/client.py
@@ -0,0 +1,101 @@
+import socket
+import os
+import sys
+import random
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+
+def client():
+    # Server Information
+    serverName = input("Enter the server IP or name: ")
+    serverPort = 13000
+
+    # Open server public key from file
+    with open('server_public.pem', 'rb') as file:
+        server_public = file.read()
+    # Generate Cyphering Block
+    server_pubkey = RSA.import_key(server_public)
+    cipher_rsa_en = PKCS1_OAEP.new(server_pubkey)
+
+    # Open client private key from file
+    with open('client1_private.pem', 'rb') as file:
+        client_private = file.read()
+    # Generate decrypting block
+    client_privkey = RSA.import_key(client_private)
+    cipher_rsa_dec = PKCS1_OAEP.new(client_privkey)
+
+    # Create client socket that useing IPv4 and TCP protocols
+    try:
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print('Error in client socket creation:', e)
+        sys.exit(1)
+
+    try:
+        # Client connect with the server
+        clientSocket.connect((serverName, serverPort))
+
+        # Client gets user info
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+        # Client sends user info
+        userEncrypted = cipher_rsa_en.encrypt(username.encode('ascii'))
+        clientSocket.send(userEncrypted)
+
+        passEncrypted = cipher_rsa_en.encrypt(password.encode('ascii'))
+        clientSocket.send(passEncrypted)
+
+        # Client receives login response from server
+        loginResponse = clientSocket.recv(2048)
+        try:
+            loginResponseString = loginResponse.decode('ascii')
+        except:
+            loginResponseString = "Valid user"
+
+        if (loginResponseString == "Invalid username or password"):
+            # Terminate the connection
+            print("Invalid username or password.\nTerminating.")
+            clientSocket.close()
+            sys.exit(0)
+
+        # Server has sent symmetric key, decrypt and store it
+        sym_key = cipher_rsa_dec.decrypt(loginResponse)
+        #print(sym_key.decode('ascii'))
+
+        # Create new cipher from symmetric key
+        cipher_symmetric = AES.new(sym_key, AES.MODE_ECB)
+
+        # Send OK response message to server
+        okMsg = "OK"
+        okMsgEncrypted = cipher_symmetric.encrypt(
+            pad(okMsg.encode('ascii'), 16))
+        clientSocket.send(okMsgEncrypted)
+
+        # Start menu loop
+        menuChoice = "0"
+        while (menuChoice != "4"):
+            # Receive menu message from server
+            menuMsgEncrypted = clientSocket.recv(2048)
+            menuMsgPadded = cipher_symmetric.decrypt(menuMsgEncrypted)
+            menuMsg = unpad(menuMsgPadded,16).decode('ascii')
+            menuChoice = input(menuMsg)
+
+            # Send menu choice to server
+            menuChoiceEncrypted = cipher_symmetric.encrypt(pad(menuChoice.encode('ascii'), 16))
+            clientSocket.send(menuChoiceEncrypted)
+
+        # Client terminate connection with the server
+        clientSocket.close()
+
+    except socket.error as e:
+        print('An error occured:', e)
+        clientSocket.close()
+        sys.exit(1)
+
+
+# ----------
+client()
---
+++ Keys/key_generator.py
@@ -0,0 +1,104 @@
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import PKCS1_OAEP
+
+
+def pairKeygen():
+    '''
+    Function to generate a pair of private and public keys. Takes no parameters and returns
+    a private and a public key in that order.
+
+    Ex. usage
+    privateKey1, publicKey1 = pairKeygen()
+    '''
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
+
+    return private_key, public_key
+
+
+# Generate server private and public keys
+serverPrivKey, serverPubKey = pairKeygen()
+
+f = open("server_private.pem", "wb")
+f.write(serverPrivKey)
+f.close()
+
+f = open("server_public.pem", "wb")
+f.write(serverPubKey)
+f.close()
+
+storeKeys = []
+# Generate client keys for all 5 clients
+for i in range(1, 6):
+    tempPriv, tempPub = pairKeygen()
+    storeKeys.append(tempPriv)
+    storeKeys.append(tempPub)
+
+# Assign client 1 private and public keys
+client1PrivKey = storeKeys[0]
+client1PubKey = storeKeys[1]
+
+# Assign client 2 private and public keys
+client2PrivKey = storeKeys[2]
+client2PubKey = storeKeys[3]
+
+# Assign client 3 private and public keys
+client3PrivKey = storeKeys[4]
+client3PubKey = storeKeys[5]
+
+# Assign client 4 private and public keys
+client4PrivKey = storeKeys[6]
+client4PubKey = storeKeys[7]
+
+# Assign client 5 private and public keys
+client5PrivKey = storeKeys[8]
+client5PubKey = storeKeys[9]
+
+# Create key files for client 1
+f = open("client1_private.pem", "wb")
+f.write(storeKeys[0])
+f.close()
+
+f = open("client1_public.pem", "wb")
+f.write(storeKeys[1])
+f.close()
+
+# Create key files for client 2
+f = open("client2_private.pem", "wb")
+f.write(storeKeys[2])
+f.close()
+
+f = open("client2_public.pem", "wb")
+f.write(storeKeys[3])
+f.close()
+
+# Create key files for client 3
+f = open("client3_private.pem", "wb")
+f.write(storeKeys[4])
+f.close()
+
+f = open("client3_public.pem", "wb")
+f.write(storeKeys[5])
+f.close()
+
+# Create key files for client 4
+f = open("client4_private.pem", "wb")
+f.write(storeKeys[6])
+f.close()
+
+f = open("client4_public.pem", "wb")
+f.write(storeKeys[7])
+f.close()
+
+# Create key files for client 5
+f = open("client5_private.pem", "wb")
+f.write(storeKeys[8])
+f.close()
+
+f = open("client5_public.pem", "wb")
+f.write(storeKeys[9])
+f.close()
+
+print("finished")
