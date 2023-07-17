ac9ec45884e10ca70ce27ae4fd4614cc101577df
Moataz Khallaf <MoatazKhallaf@outlook.com>
2023-07-17 12:57:27 -0600

    - code reformat functions to classes
    Need to push to swap over to WSL because fork doesn't work on windows T_T

--- Client1/client.py
+++ Client1/client.py
@@ -8,95 +8,202 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 
-def client():
-    # Server Information
-    serverName = input("Enter the server IP or name: ")
-    serverPort = 13000
+###############################################################################
+class Client:
+    def __init__(self, serverName="127.0.0.1"):
+        # Server Information
+        self.serverName = serverName
+        self.serverPort = 13000
+        self.server_public = None
+        self.server_pubkey = None
+        self.cipher_rsa_en = None
+        # Client Information
+        self.clientSocket = None
+        self.client_private = None
+        self.client_privkey = None
+        self.cipher_rsa_dec = None
+        self.loginResponse = None
+        # Symmetric Key
+        self.cipher_symmetric = None
 
-    # Open server public key from file
-    with open("server_public.pem", "rb") as file:
-        server_public = file.read()
-    # Generate Cyphering Block
-    server_pubkey = RSA.import_key(server_public)
-    cipher_rsa_en = PKCS1_OAEP.new(server_pubkey)
+    # LOGGING IN
 
-    # Open client private key from file
-    with open("client1_private.pem", "rb") as file:
-        client_private = file.read()
-    # Generate decrypting block
-    client_privkey = RSA.import_key(client_private)
-    cipher_rsa_dec = PKCS1_OAEP.new(client_privkey)
+    def fill_server_public(self):
+        """
+        Fill server public key from file
+        Paremeters: None
+        Return: None
+        """
+        # Open server public key from file
+        with open("server_public.pem", "rb") as file:
+            self.server_public = file.read()
+        # Generate Cyphering Block
+        self.server_pubkey = RSA.import_key(self.server_public)
+        self.cipher_rsa_en = PKCS1_OAEP.new(self.server_pubkey)
+        return
 
-    # Create client socket that useing IPv4 and TCP protocols
-    try:
-        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error as e:
-        print("Error in client socket creation:", e)
-        sys.exit(1)
+    def fill_client_private(self):
+        """
+        Fill client private key from file
+        Paremeters: None
+        Return: None
+        """
+        # Open client private key from file
+        with open("client1_private.pem", "rb") as file:
+            self.client_private = file.read()
+        # Generate decrypting block
+        self.client_privkey = RSA.import_key(self.client_private)
+        self.cipher_rsa_dec = PKCS1_OAEP.new(self.client_privkey)
+        return
 
-    try:
+    def create_client_socket(self):
+        """
+        Create client socket
+        Paremeters: None
+        Return: None
+        """
+        # Create client socket that useing IPv4 and TCP protocols
+        try:
+            self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as e:
+            print("Error in client socket creation:", e)
+            sys.exit(1)
+        return
+
+    def connect_to_server(self):
+        """
+        Connect to server
+        Paremeters: None
+        Return: None
+        """
         # Client connect with the server
-        clientSocket.connect((serverName, serverPort))
+        self.clientSocket.connect((self.serverName, self.serverPort))
+        return
 
+    def send_user_info(self):
+        """
+        Send user info to server
+        Paremeters: None
+        Return: None
+        """
         # Client gets user info
         username = input("Enter your username: ")
         password = input("Enter your password: ")
 
         # Client sends user info
-        userEncrypted = cipher_rsa_en.encrypt(username.encode("ascii"))
-        clientSocket.send(userEncrypted)
+        userEncrypted = self.cipher_rsa_en.encrypt(username.encode("ascii"))
+        self.clientSocket.send(userEncrypted)
 
-        passEncrypted = cipher_rsa_en.encrypt(password.encode("ascii"))
-        clientSocket.send(passEncrypted)
+        passEncrypted = self.cipher_rsa_en.encrypt(password.encode("ascii"))
+        self.clientSocket.send(passEncrypted)
+        return
 
-        # Client receives login response from server
-        loginResponse = clientSocket.recv(2048)
+    def receive_user_info(self):
+        """
+        Receive user info from server
+        Paremeters: None
+        Return: None
+        """
+        loginResponse = self.clientSocket.recv(2048)
         try:
-            loginResponseString = loginResponse.decode("ascii")
+            loginResponseString = self.cipher_rsa_dec.decrypt(loginResponse)
         except:
-            loginResponseString = "Valid user"
+            loginResponseString == "Valid User"
+            self.loginResponse = loginResponse
 
         if loginResponseString == "Invalid username or password":
             # Terminate the connection
             print("Invalid username or password.\nTerminating.")
-            clientSocket.close()
+            self.clientSocket.close()
             sys.exit(0)
 
+    def receive_symmetric_key(self):
+        """
+        Receive symmetric key from server
+        Paremeters: None
+        Return: None
+        """
         # Server has sent symmetric key, decrypt and store it
-        sym_key = cipher_rsa_dec.decrypt(loginResponse)
-        # print(sym_key.decode('ascii'))
+        sym_key = self.cipher_rsa_dec.decrypt(self.loginResponse)
 
         # Create new cipher from symmetric key
         cipher_symmetric = AES.new(sym_key, AES.MODE_ECB)
+        self.cipher_symmetric = cipher_symmetric
 
+    def send_ok_response(self):
+        """
+        Send OK response to server
+        Paremeters: None
+        Return: None
+        """
         # Send OK response message to server
         okMsg = "OK"
-        okMsgEncrypted = cipher_symmetric.encrypt(pad(okMsg.encode("ascii"), 16))
-        clientSocket.send(okMsgEncrypted)
+        okMsgEncrypted = self.cipher_symmetric.encrypt(pad(okMsg.encode("ascii"), 16))
+        self.clientSocket.send(okMsgEncrypted)
+        return
 
-        # Start menu loop
+    def login(self):
+        """
+        Login to server
+        Paremeters: None
+        Return: None
+        """
+        self.fill_server_public()
+        self.fill_client_private()
+        self.create_client_socket()
+        self.connect_to_server()
+        self.send_user_info()
+        self.receive_user_info()
+        self.send_ok_response()
+        return
+
+    # MENU
+
+    def menu_loop(self):
+        """
+        Menu loop
+        Paremeters: None
+        Return: None
+        """
         menuChoice = "0"
+
         while menuChoice != "4":
-            # Receive menu message from server
-            menuMsgEncrypted = clientSocket.recv(2048)
-            menuMsgPadded = cipher_symmetric.decrypt(menuMsgEncrypted)
+            # Receive menu from server
+            menuMsgEncrypted = self.clientSocket.recv(2048)
+            menuMsgPadded = self.cipher_symmetric.decrypt(menuMsgEncrypted)
             menuMsg = unpad(menuMsgPadded, 16).decode("ascii")
-            menuChoice = input(menuMsg)
+            manuChoice = input(menuMsg)
 
             # Send menu choice to server
-            menuChoiceEncrypted = cipher_symmetric.encrypt(
-                pad(menuChoice.encode("ascii"), 16)
-            )
-            clientSocket.send(menuChoiceEncrypted)
+            self.send_menu_choice(self.cipher_symmetric, menuChoice)
 
-        # Client terminate connection with the server
-        clientSocket.close()
+        self.clientSocket.close()
 
-    except socket.error as e:
-        print("An error occured:", e)
-        clientSocket.close()
-        sys.exit(1)
+    def send_menu_choice(self, menuChoice):
+        """
+        Send menu choice to server
+        Paremeters: cipher_symmetric, menuChoice
+        Return: None
+        """
+        menuChoiceEncrypted = self.cipher_symmetric.encrypt(
+            pad(menuChoice.encode("ascii"), 16)
+        )
+        self.clientSocket.send(menuChoiceEncrypted)
+
+        return
 
 
-# ----------
-client()
+###############################################################################
+
+
+def main():
+    # TODO change this to input after testing
+    client = Client("127.0.0.1")
+    client.login()
+    client.menu_loop()
+
+
+###############################################################################
+
+if __name__ == "__main__":
+    main()
--- Server/server.py
+++ Server/server.py
@@ -9,147 +9,192 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 
+###############################################################################
 
-def server():
-    # Server port
-    serverPort = 13000
 
-    # Create server socket that uses IPv4 and TCP protocols
-    try:
-        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error as e:
-        print("Error in server socket creation:", e)
-        sys.exit(1)
+class Server:
+    def __init__(self):
+        self.serverPort = 13000
+        self.serverSocket = None
+        self.server_public = None
+        self.server_pubkey = None
+        self.cipher_rsa_en_server = None
+        self.server_private = None
+        self.server_privkey = None
+        self.cipher_rsa_dec_server = None
+        self.cipher_symmetric = None
 
-    # Associate 13000 port number to the server socket
-    try:
-        serverSocket.bind(("", serverPort))
-    except socket.error as e:
-        print("Error in server socket binding:", e)
-        sys.exit(1)
-
-    print("The server is ready to accept connections")
-
-    # The server can only have one connection in its queue waiting for acceptance
-    serverSocket.listen(5)
-
-    while 1:
+    def create_server_socket(self):
+        """
+        Create server socket that uses IPv4 and TCP protocols
+        Paremeters: None
+        Return: None
+        """
         try:
-            # Server accepts client connection
-            connectionSocket, addr = serverSocket.accept()
+            self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as e:
+            print("Error in server socket creation:", e)
+            sys.exit(1)
+        return
 
-            with open("server_public.pem", "rb") as file:
-                server_public = file.read()
-            # Generate Cyphering Block
-            server_pubkey = RSA.import_key(server_public)
-            cipher_rsa_en_server = PKCS1_OAEP.new(server_pubkey)
+    def bind_server_socket(self):
+        """
+        Associate 13000 port number to the server socket
+        Paremeters: None
+        Return: None
+        """
+        try:
+            self.serverSocket.bind(("", self.serverPort))
+        except socket.error as e:
+            print("Error in server socket binding:", e)
+            sys.exit(1)
 
-            # Open server private key from file
-            with open("server_private.pem", "rb") as file:
-                server_private = file.read()
-            # Generate decrypting block
-            server_privkey = RSA.import_key(server_private)
-            cipher_rsa_dec_server = PKCS1_OAEP.new(server_privkey)
+        print("The server is ready to accept connections")
+        return
 
-            pid = os.fork()
+    def fill_server_public(self):
+        """
+        Fill server public key from file
+        Paremeters: None
+        Return: None
+        """
+        # Open server public key from file
+        with open("server_public.pem", "rb") as file:
+            self.server_public = file.read()
+        # Generate Cyphering Block
+        self.server_pubkey = RSA.import_key(self.server_public)
+        self.cipher_rsa_en_server = PKCS1_OAEP.new(self.server_pubkey)
+        return
 
-            # If it is a client process
-            if pid == 0:
-                serverSocket.close()
+    def fill_server_private(self):
+        """
+        Fill server private key from file
+        Paremeters: None
+        Return: None
+        """
+        # Open server private key from file
+        with open("server_private.pem", "rb") as file:
+            self.server_private = file.read()
+        # Generate decrypting block
+        self.server_privkey = RSA.import_key(self.server_private)
+        self.cipher_rsa_dec_server = PKCS1_OAEP.new(self.server_privkey)
+        return
 
-                # Receive user info from client
-                userEncrypted = connectionSocket.recv(2048)
-                username = cipher_rsa_dec_server.decrypt(userEncrypted)
-                username = username.decode("ascii")
+    def server_loop(self):
+        self.serverSocket.listen(5)
 
-                passEncrypted = connectionSocket.recv(2048)
-                password = cipher_rsa_dec_server.decrypt(passEncrypted)
-                password = password.decode("ascii")
+        while 1:
+            try:
+                # Server accepts client connection
+                self.connectionSocket, addr = self.serverSocket.accept()
 
-                # Check against json file to determine if valid user
-                validUser = 0
-                f = open("user_pass.json")
-                userdata = json.load(f)
+                self.fill_server_public()
+                self.fill_server_private()
 
-                for user in userdata:
-                    if user == username:
-                        if password == userdata[username]:
-                            validUser = 1
+                pid = os.fork()
 
-                f.close()
+                # If it is a client process
+                if pid == 0:
+                    self.serverSocket.close()
 
-                if validUser == 1:
-                    # Valid user
-                    # Create sym key and send
-                    KeyLen = 256
-                    sym_key = get_random_bytes(int(KeyLen / 8))
-                    # Generate Cyphering Block for later usage
-                    cipher_symmetric = AES.new(sym_key, AES.MODE_ECB)
+                    # Receive user info from client
+                    userEncrypted = self.connectionSocket.recv(2048)
+                    username = self.cipher_rsa_dec_server.decrypt(userEncrypted)
+                    username = username.decode("ascii")
 
-                    # Encrypt the symmetric key and send to client with their respective public key
-                    clientPubFilename = username + "_public.pem"
-                    # Open client key file
-                    with open(clientPubFilename, "rb") as file:
-                        client_public = file.read()
-                    # Generate Cyphering Block
-                    client_pubkey = RSA.import_key(client_public)
-                    cipher_rsa_en_client = PKCS1_OAEP.new(client_pubkey)
+                    passEncrypted = self.connectionSocket.recv(2048)
+                    password = self.cipher_rsa_dec_server.decrypt(passEncrypted)
+                    password = password.decode("ascii")
 
-                    sym_key_encrypted = cipher_rsa_en_client.encrypt(sym_key)
-                    connectionSocket.send(sym_key_encrypted)
+                    # Check against json file to determine if valid user
+                    validUser = 0
+                    f = open("user_pass.json")
+                    userdata = json.load(f)
 
-                    # Print confirmation message
-                    print(
-                        f"Connection Accepted and Symmetric Key Generated for client: {username}"
-                    )
+                    for user in userdata:
+                        if user == username:
+                            if password == userdata[username]:
+                                validUser = 1
 
-                    # Receive OK message from client
-                    okMsgEncrypted = connectionSocket.recv(2048)
-                    okPadded = cipher_symmetric.decrypt(okMsgEncrypted)
-                    okMsg = unpad(okPadded, 16)
+                    f.close()
 
-                    # Start menu loop
-                    menuChoice = "0"
-                    while menuChoice != "4":
-                        # Send client menu message
-                        menuMsg = (
-                            "Select the operation:\n1) Create and send an email\n2) Display the inbox\n"
-                            "3) Display the email contents\n4) Terminate the connection\nchoice: "
+                    if validUser == 1:
+                        # Create sym key and send
+                        keyLen = 256
+                        sym_key = get_random_bytes(int(keyLen / 8))
+
+                        # Generate Cyphering Block
+                        self.cipher_symmetric = AES.new(sym_key, AES.MODE_CBC)
+
+                        # encrypt sym key with server public key
+                        clientPubFilename = username + "_public.pem"
+                        with open(clientPubFilename, "rb") as file:
+                            client_public = file.read()
+                        client_pubkey = RSA.import_key(client_public)
+                        cipher_rsa_en_client = PKCS1_OAEP.new(client_pubkey)
+
+                        symEncrypted = cipher_rsa_en_client.encrypt(sym_key)
+                        self.connectionSocket.send(symEncrypted)
+
+                        print(
+                            f"Connection Accepted and Symmetric Key Generated for client: {username}"
                         )
-                        menuMsgEncrypted = cipher_symmetric.encrypt(
+
+                        # Receive OK Msg from Client
+                        okMsgEncrypted = self.connectionSocket.recv(2048)
+                        okPadded = self.cipher_symmetric.decrypt(okMsgEncrypted)
+                        okMsg = unpad(okPadded, AES.block_size)
+
+                        # Start menu loop
+                        menuChoice = "0"
+                        while menuChoice != "4":
+                            menuMsg = (
+                                "Select the operation:\n1) Create and send an email\n2) Display the inbox\n"
+                                "3) Display the email contents\n4) Terminate the connection\nchoice: "
+                            )
+                        menuMsgEncrypted = self.cipher_symmetric.encrypt(
                             pad(menuMsg.encode("ascii"), 16)
                         )
-                        connectionSocket.send(menuMsgEncrypted)
+                        self.connectionSocket.send(menuMsgEncrypted)
 
                         # Receive menu choice from client
-                        menuChoiceEncrypted = connectionSocket.recv(2048)
-                        menuChoicePadded = cipher_symmetric.decrypt(menuChoiceEncrypted)
+                        menuChoiceEncrypted = self.connectionSocket.recv(2048)
+                        menuChoicePadded = self.cipher_symmetric.decrypt(
+                            menuChoiceEncrypted
+                        )
                         menuChoice = unpad(menuChoicePadded, 16).decode("ascii")
 
-                if validUser == 0:
-                    # Invalid user, send termination notice
-                    invalidMsg = "Invalid username or password"
-                    connectionSocket.send(invalidMsg.encode("ascii"))
-                    print(
-                        f"The received client information: {username} is invalid (Connection Terminated)."
-                    )
+                    if validUser == 0:
+                        # Invalid user, send termination notice
+                        invalidMsg = "Invalid username or password"
+                        self.connectionSocket.send(invalidMsg.encode("ascii"))
+                        print(
+                            f"The received client information: {username} is invalid (Connection Terminated)."
+                        )
 
-                connectionSocket.close()
+                    self.connectionSocket.close()
 
-                return
+                    return
+
+            except socket.error as e:
+                print("An error occured:", e)
+                self.serverSocket.close()
+                sys.exit(1)
 
             # Parent doesn't need this connection
-            connectionSocket.close()
-
-        except socket.error as e:
-            print("An error occured:", e)
-            serverSocket.close()
-            sys.exit(1)
-        except:
-            print("Goodbye")
-            serverSocket.close()
-            sys.exit(0)
+            self.connectionSocket.close()
 
 
-# -------
-server()
+###############################################################################
+
+
+def main():
+    server = Server()
+    server.create_server_socket()
+    server.bind_server_socket()
+    server.server_loop()
+
+
+###############################################################################
+if __name__ == "__main__":
+    main()
